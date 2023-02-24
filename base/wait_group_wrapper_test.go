27840bfaffe1a01b14aa961f7818a29332dfa5c9
feixiao <feixiao2020@sina.com>
2023-02-24 13:27:58 +0800

    add WaitGroupWrapper

---
+++ base/wait_group_wrapper_test.go
@@ -0,0 +1,15 @@
+package base
+
+import (
+	"testing"
+	"time"
+)
+
+func TestWaitGroup(t *testing.T) {
+	var wg WaitGroupWrapper
+	wg.Wrap(func() {
+		time.Sleep(2 * time.Millisecond)
+	})
+
+	wg.Wait()
+}
